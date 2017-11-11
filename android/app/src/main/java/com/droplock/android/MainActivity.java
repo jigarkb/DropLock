f5f223bb0e8e9f7e522349555367f339a94b10d8
Jigar Bhatt <jigarbhatt93@gmail.com>
2017-11-10 21:23:14 -0800

    android skeleton

---
+++ android/app/src/main/java/com/droplock/android/MainActivity.java
@@ -0,0 +1,47 @@
+package com.droplock.android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView wvPublic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wvPublic = (WebView) findViewById(R.id.wvPublic);
+        wvPublic.getSettings().setJavaScriptEnabled(true);
+        wvPublic.loadUrl("https://drop-lock.appspot.com");
+        wvPublic.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.toString());
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (wvPublic.canGoBack()) {
+                        wvPublic.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
